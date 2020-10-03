89c22ca0196b4c15a0a8d746b31983229fefeeef
AlfaLucky <shiryaaa@mail.ru>
2020-10-03 21:59:32 +0300

    feat: Added solution for typical-array-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
 exports.min = function min (array) {
-  return 0;
+   if (typeof array === 'object' && array.length !== 0) {
+     let res = array[0];
+     array.forEach(element => {
+       if (element < res) {
+         res = element;
+       }
+     });
+     return res;
+  } else {
+     return 0;
 }
-
+}
+  
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'object' && array.length !== 0) {
+    let res = array[0];
+    array.forEach(element => {
+      if (element > res) {
+        res = element;
+      }
+    });
+    return res;
+  } else {
+    return 0;
+  }
 }
-
+  
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'object' && array.length !== 0) {
+    let res = 0;
+    array.forEach(element => {
+      res += element;
+    });
+    return res / array.length;
+  } else {
+    return 0;
+  }
 }
